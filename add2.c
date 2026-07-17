f5bc45a29083a3d08df03c018fb2cdecd39d2d92
shwetaamali07 <shwetaamali07@gmail.com>
2026-07-17 10:50:38 +0530

    Add files via upload

---
+++ add2.c
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,add,sub,multi,div,mod;
+cout<<"enter two numbers";
+cin>>a>>b;
+add=a+b;
+sub=a-b;
+multi=a*b;
+div=a/b;
+mod=a%b;
+cout<<"sum="<<add<<endl;
+cout<<"sub="<<sub<<endl;
+cout<<"multi="<<multi<<endl;
+cout<<"div="<<div<<endl;
+cout<<"mod="<<mod<<endl;
+return 0;
+}
